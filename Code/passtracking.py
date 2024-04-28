851900c4e51fb3e6506e75620b485bd8177cb9ce
YSangtani <sangtaniyash74@gmail.com>
2024-04-28 15:30:47 +0530

    Extracted possessions using the shot clock.

--- Code/passtracking.py
+++ Code/passtracking.py
@@ -28,6 +28,7 @@ class PassTracking:
         self.ball_man = int(data.pbp.loc[data.pbp['EVENTMSGTYPE'] == 10]['PLAYER3_ID'].iloc[0])
         self.pass_attempt = None
         self.moments = data.moments
+        self.pbp = data.pbp
 
     def _dist_calculate(self, x1, y1, x2, y2):
         return ((x1 - x2)**2 + (y1 - y2)**2)**0.5
@@ -44,7 +45,33 @@ class PassTracking:
                 player_id_with_ball = (player[0], player[1])  # team_id, player_id
 
         return player_id_with_ball[0], player_id_with_ball[1]    
+
     def track_passes(self):
+
+        """While the shot cloc is less than twenty four, and the player id changes, pass successful. 
+        If the shot clock resets to twenty-four and it wasn't a shot attempt, shot made, foul, violation, etc. (will get to knoow this from the event_num), 
+        PASS UNSUCCESSFUL.
+        
+
+        # Modify getting_data.py to have event_num and event_type.
+        Write the code to use the logic written above.
+        Run the code.
+        Calculate the features.
+        Run the model.
+        """
+        possessions = self.moments.loc[((self.moments['shot_clock']==24.0) & (self.moments['shot_clock'] - self.moments['shot_clock'].shift(1) != 0.0)) |
+                                       ((self.moments['shot_clock'] < 24.0) & (self.moments['shot_clock'].shift(-1) == 24.0))]
+        possessions = possessions.loc[(possessions['shot_clock']==24.0) |(possessions['shot_clock'] <23.0)]
+        possessions = possessions.loc[((possessions['shot_clock']==24.0) &(possessions['shot_clock'].shift(-1) != 24.0))|
+                                      (possessions['shot_clock']!=24.0)]
+
+        
+        self.pass_attempt = possessions
+        
+        possessions = possessions['shot_clock']
+        print(possessions)
+
+        """
         self.moments[['ball_team', 'ball_man']] = self.moments['positions'].apply(self.find_ball_man).apply(pd.Series)
         self.moments['prev_ball_man'] = self.moments['ball_man'].shift(1)
         self.moments['prev_ball_team'] = self.moments['ball_team'].shift(1)
@@ -56,7 +83,7 @@ class PassTracking:
                             'prev_2_ball_man', 'prev_2_ball_team', 'prev_3_ball_man', 'prev_3_ball_team']
         self.moments.dropna(subset=required_columns, inplace=True)
         self.moments['pass_successful'] = np.where(self.moments['ball_team'] != self.moments['prev_2_ball_team'], 0, 1)
-        
+        print(len(self.moments.loc[self.moments['pass_successful'] == 1]))
         condition1 = self.moments['ball_man'].ne(self.moments['prev_ball_man'])
         condition2 = self.moments['ball_man'].ne(self.moments['prev_2_ball_man'])
 
@@ -64,14 +91,17 @@ class PassTracking:
         self.pass_attempt = self.moments.loc[condition1 | condition2]
 
         #self.pass_attempt = self.moments.loc[self.moments['ball_man'] != 
-        #                                    self.moments['prev_ball_man'] | self.moments['prev_2_ball_man']]
+        #                                     self.moments['prev_ball_man'] | self.moments['prev_2_ball_man']]
 
-
+        """
 if __name__ == '__main__':
-    passes = PassTracking('12.30.2015', 'SAC', 'PHI')
+    passes = PassTracking('12.25.2015', 'GSW', 'CLE')
     passes.track_passes()
-    passes.pass_attempt.to_csv('../Tracking_data/passes/passes.csv')
+    passes.moments.to_csv('../Tracking_data/passes/passes.csv')
+    passes.pass_attempt.to_csv('../Tracking_data/passes/possessions.csv')
+    passes.pbp.to_csv('../Tracking_data/passes/pbp.csv')
+    #passes.pass_attempt.to_csv('../Tracking_data/passes/passes.csv')
     print("CSV file saved and ready to use.")
     print(passes.pass_attempt)
-    
+    print(passes.pbp)
     #12.30.2015.PHI.at.SAC.7z
